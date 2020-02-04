b971a5b237ab1f5ba5d645ad0965378545964aef
John C <jcaraballo019@gmail.com>
2020-02-03 23:08:25 -0400

    added user auth functionality partial

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { UserRepository } from './user.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        @InjectRepository(UserRepository) private userRepository: UserRepository
+    ) {}
+
+    signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        return this.userRepository.signUp(authCredentialsDto);
+    }
+}
---
+++ src/auth/user.repository.ts
@@ -0,0 +1,29 @@
+import { Repository, EntityRepository } from 'typeorm';
+import {
+    ConflictException,
+    InternalServerErrorException
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+    async signUp({ username, password }: AuthCredentialsDto): Promise<void> {
+        const salt = await bcrypt.genSalt();
+
+        const user = new User();
+        user.username = username;
+        user.password = password;
+
+        try {
+            await user.save();
+        } catch (error) {
+            if (error.code === '23505') {
+                throw new ConflictException('Username already exists');
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
+    }
+}
