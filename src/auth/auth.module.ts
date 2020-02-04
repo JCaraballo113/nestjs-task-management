2b0613c804f46709c74b168d27ef16a0474b07c9
John C <jcaraballo019@gmail.com>
2020-02-04 10:39:50 -0400

    set up env variables

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,12 +6,15 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRepository } from './user.repository';
 import { JwtStrategy } from './jwt.strategy';
+import * as config from 'config';
+
+const jwtConfig = config.get('jwt');
 
 @Module({
     imports: [
         PassportModule.register({ defaultStrategy: 'jwt' }),
         JwtModule.register({
-            secret: 'topSecret51',
+            secret: process.env.JWT_SECRET || jwtConfig.secret,
             signOptions: {
                 expiresIn: 3600
             }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
+
+interface ServerConfig {
+    port?: string;
+}
 
 async function bootstrap() {
+    const serverConfig: ServerConfig = config.get('server');
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    await app.listen(process.env.PORT || serverConfig.port);
 }
 bootstrap();
