0e908134c1764db2f082ce6c5817e05e59e0cec6
John C <jcaraballo019@gmail.com>
2020-02-04 10:45:29 -0400

    cors setup

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,10 @@ interface ServerConfig {
 async function bootstrap() {
     const serverConfig: ServerConfig = config.get('server');
     const app = await NestFactory.create(AppModule);
+
+    if (process.env.NODE_ENV === 'development') {
+        app.enableCors();
+    }
     await app.listen(process.env.PORT || serverConfig.port);
 }
 bootstrap();
